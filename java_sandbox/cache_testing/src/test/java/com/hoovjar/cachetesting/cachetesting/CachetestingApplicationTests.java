a6579b4229ab87bc6dc36b51e6e8c111680687da
Jarrod Hoover <hoov85@gmail.com>
2024-11-22 11:20:09 -0500

    initial commit

---
+++ java_sandbox/cache_testing/src/test/java/com/hoovjar/cachetesting/cachetesting/CachetestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hoovjar.cachetesting.cachetesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CachetestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
